5ccd14ddde464e65110df01ac414b6bf05fb122d
Nefi S. Yendes <nefi.s.yendes@gmail.com>
2025-01-10 19:40:33 -0400

    Proyecto de cambio de colores aleatorios

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
-function normal() {
+document.body.addEventListener("keydown", (event) => {
+    let con = 1;
+    setInterval(() =>{
+        if(event.key != "Enter" && con == 1){
+            let r= Math.round(Math.random() * 256);
+            let g = Math.round(Math.random() * 256);
+            let b = Math.round(Math.random() * 256);
+            document.body.style.backgroundColor = `rgb(${r},${g},${b})`;
     
-}
+            let hr= Math.round(Math.random() * 256);
+            let hg = Math.round(Math.random() * 256);
+            let hb = Math.round(Math.random() * 256);
+            document.getElementById("h1").style.color = `rgb(${hr},${hg},${hb})`;
+        }
+        con++;
+    }, 1000);
+});
